46100e640ea42c59150a9ec420b3340cd499823b
Tomas Januskevic <tomjanuskevic@gmail.com>
2021-07-03 11:39:04 +0300

    added data types

---
+++ src/main/java/dataTypes/DataTypesMain.java
@@ -0,0 +1,20 @@
+package dataTypes;
+
+public class DataTypesMain {
+    byte bitas = 127;
+    short streetNumber = 5;
+    int age = 30;
+    long salary = 2000;
+
+    float height = 1.84f;
+    double weight = 90.90;
+
+    boolean isMaried = true;
+
+    char creditClass = 'A';
+
+    int[] gatvetNumeriai = new int[4];
+
+    String vardas = "Hello";
+
+}
